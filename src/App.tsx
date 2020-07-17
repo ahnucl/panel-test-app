46d441f357d7d52539174d80bd2c3f41ee72918b
Leonardo Cunha <lalmcunha@gmail.com>
2020-07-17 13:54:52 -0300

    iterando paineis do arquivo local

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import GlobalStyle from './styles/global';
 
-import PanelList from './components/PanelList';
-
 import Routes from './routes';
 
 const App: React.FC = () => {
--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import data from '../../data/panel-data.json';
 
 /**
  * Fazer do jeito que eu sei, com Hooks, e depois alterar
  * para usar Redux
  */
-const Dashboard: React.FC = () => <h1>Dashboard Page</h1>;
+
+interface PanelColumn {
+  name: string;
+  color: string;
+}
+
+interface Panel {
+  name: string;
+  columns: PanelColumn[];
+}
+
+const Dashboard: React.FC = () => {
+  const [panels] = useState<Panel[]>(data.panels);
+
+  // useEffect(() => {}, []);
+
+  return (
+    <>
+      <h1>Dashboard Page</h1>
+      {panels.map(panel => (
+        <a key={panel.name} href={`panel/${panel.name}`}>
+          {panel.name}
+        </a>
+      ))}
+    </>
+  );
+};
 
 export default Dashboard;
